b7c7cd359e1e713ad3828e687e6125543f14b174
vikram kumar mehta <vkm.kush@gmail.com>
2026-06-30 11:06:43 +0530

    Variables and data types

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,13 @@
-console.log("Apna College");
-console.log("Vikram Kushwaha");
+// age = 24;
+// console.log(age+1);
+// fullname = "Tony Stark";
+// console.log(fullname);
+// fullName = "Vikram Kushwha";
+// console.log(fullName);
+
+let fullName = "Vikram Kushwaha";
+let age = 24;
+let totalPrice = 1000;
+const pi=3.14;
+console.log(fullName);
+console.log(pi);
